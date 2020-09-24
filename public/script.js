677acb4067f3996e85a9cccaa10f06b8b790114e
rohit236c <rohit236c@gmail.com>
2020-09-24 21:39:00 +0530

    stop button functioning added
    Signed-off-by: rohit236c <rohit236c@gmail.com>

--- public/script.js
+++ public/script.js
@@ -95,3 +95,34 @@ const setUnmuteButton = () => {
         .querySelector('.main__mute_button')
         .innerHTML = html;
 }
+//stop video
+const playStop = () => {
+    // console.log('object')
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document
+        .querySelector('.main__video_button')
+        .innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>
+    `
+    document
+        .querySelector('.main__video_button')
+        .innerHTML = html;
+}
